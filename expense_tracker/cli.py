5ce3e1f9d2bf2c0cefb94f62b6aa173e64970ce9
Blokz101 <mquantwenty22@gmail.com>
2023-07-04 03:56:42 -0400

    Updated cli.py to use a class instead of random functions

--- expense_tracker/cli.py
+++ expense_tracker/cli.py
@@ -9,57 +9,68 @@ from expense_tracker import __app_name__, __version__
 
 
 
-app = typer.Typer()
-
-
-
-def _version_callback(value: bool) -> None:
+class CLI:
     """
-    Callback for main command and version option.
+    ViewController for the application. Deals with command line user interaction.
     """
-    if value:
-        typer.echo(f"{__app_name__} {__version__}")
-        raise typer.Exit()
+
+
+
+    app: typer.Typer = typer.Typer()
+
+
+
+    def _version_callback(value: bool) -> None:
+        """
+        Callback for main command and version option.
+        """
+        if value:
+            typer.echo(f"{__app_name__} {__version__}")
+            raise typer.Exit()
+        
+        
     
-    
-    
-@app.callback()
-def main(
-    version: Annotated[bool, typer.Option(
-        help = "Display the version and exit.",
-        callback = _version_callback,
-        is_eager = True,
-    )] = False
-) -> None:
-    """
-    Reconcile and track expenses using receipt photos and bank statements.
-    """
-    
-    pass
+    @staticmethod
+    @app.callback()
+    def main(
+        version: Annotated[bool, typer.Option(
+            help = "Display the version and exit.",
+            callback = _version_callback,
+            is_eager = True,
+        )] = False
+    ) -> None:
+        """
+        Reconcile and track expenses using receipt photos and bank statements.
+        """
+
+        pass
 
 
 
-@app.command()
-def reconcile() -> None:
-    """
-    Reconcile new transactions with bank statement.
-    """
-    
-    print("Reconcile")
+    @staticmethod
+    @app.command()
+    def add(
+        photo: Annotated[bool, typer.Option(
+            help = "Create transactions based on photos."
+        )] = True,
+        auto: Annotated[bool, typer.Option(
+            help = "Automatically create transactions based on photos and bank statement."
+        )] = False,
+    ) -> None:
+        """
+        Create new transactions.
+        """
+        
+        print("Add")
 
 
 
-@app.command()
-def add(
-    photo: Annotated[bool, typer.Option(
-        help = "Create transactions based on photos."
-    )] = True,
-    auto: Annotated[bool, typer.Option(
-        help = "Automatically create transactions based on photos and bank statement."
-    )] = False,
-) -> None:
-    """
-    Create new transactions.
-    """
-    
-    print("Add")
+    @staticmethod
+    @app.command()
+    def reconcile() -> None:
+        """
+        Reconcile new transactions with bank statement.
+        """
+        
+        print("Reconcile")
+        
