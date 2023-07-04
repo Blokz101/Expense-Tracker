48b299ead87bc28f8d60ffd862e7dff617697e32
Blokz101 <mquantwenty22@gmail.com>
2023-07-04 03:57:19 -0400

    Added model.py structure

---
+++ expense_tracker/model.py
@@ -0,0 +1,15 @@
+# expense_tracker/model.py
+
+
+
+class database:
+    
+    
+    
+    def __init__(self, database_path: str) -> None:
+        """
+        Constructor
+        """
+        
+        self.database_path: str = database_path
+        
