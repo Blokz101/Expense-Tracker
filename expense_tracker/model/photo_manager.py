aabe8e747b024d70712bbed34cc480d95d8b6989
Blokz101 <mquantwenty22@gmail.com>
2023-07-19 18:38:25 -0400

    Combined the single function from coords_utils.py into Photo_Manager

--- expense_tracker/model/photo_manager.py
+++ expense_tracker/model/photo_manager.py
@@ -1,10 +1,10 @@
 # expense_tracker/model/photo_coords.py
 
+from typing import Tuple
+
 from exif import Image
 
 from pathlib import Path
-
-from expense_tracker.model.coords_utils import Coords_Utils
 
 
 class Photo_Manager:
@@ -25,11 +25,23 @@ class Photo_Manager:
                 AttributeError("Image does not have exif data.")
 
             return (
-                Coords_Utils.decimal_coords(image.gps_latitude, image.gps_latitude_ref),
-                Coords_Utils.decimal_coords(
+                Photo_Manager._to_decimal_coords(image.gps_latitude, image.gps_latitude_ref),
+                Photo_Manager._to_decimal_coords(
                     image.gps_longitude, image.gps_longitude_ref
                 ),
             )
+            
+    @staticmethod
+    def _to_decimal_coords(coords: Tuple[float, float], ref: str):
+        """
+        Convert coords and ref to decimal degrees format
+        """
+
+        decimal_degrees: float = coords[0] + coords[1] / 60 + coords[2] / 3600
+        if ref == "S" or ref == "W":
+            decimal_degrees = -decimal_degrees
+
+        return decimal_degrees
 
     @staticmethod
     def get_description(path: Path) -> str:
